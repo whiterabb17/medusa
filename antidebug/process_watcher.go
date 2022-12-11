27f52037c4f3d71c1969270bb3ac363922733892
s1l3nt78 <90134688+whiterabb17@users.noreply.github.com>
2022-12-11 10:28:18 +0200

    init

---
+++ antidebug/process_watcher.go
@@ -0,0 +1,40 @@
+package antidebug
+
+import (
+	"github.com/whiterabb17/medusa/utils"
+	//	"fmt"
+	s "strings"
+
+	ps "github.com/mitchellh/go-ps"
+)
+
+func processList() bool {
+	processList, _ := ps.Processes()
+
+	for x := range processList {
+		var process ps.Process
+		process = processList[x]
+		pn := s.ToLower(process.Executable())
+
+		if utils.PList(utils.ProcessListToCheck, pn) {
+			//			fmt.Println("PROCESS_WATCHER:" + pn)
+			return true
+		}
+	}
+	return false
+}
+
+/*
+*
+
+	Look for suspicious process running
+	Example:
+	if antidebug.ByProcessWatcher() {
+		// exit or wait
+	}
+
+*
+*/
+func ByProcessWatcher() bool {
+	return processList()
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/whiterabb17/medusa
+
+go 1.16
+
+require (
+	github.com/StackExchange/wmi v0.0.0-20210224194228-fe8f1750fd46
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/mitchellh/go-ps v1.0.0
+)
